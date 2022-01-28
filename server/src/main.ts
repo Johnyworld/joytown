a413bc2cc29297a68b9389296aca49d87249c7a9
johnyworld <johnyworld@naver.com>
2022-01-28 22:30:22 +0900

    Add enable cors for development

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,10 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
 
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors(corsOptions);
   const PORT = process.env.PORT;
   await app.listen(PORT);
 }
