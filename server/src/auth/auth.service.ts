5b0f1e47b1943016e1d34a7620d203b8266d1d26
johnyworld <johnyworld@naver.com>
2022-01-28 18:14:31 +0900

    Clean up some files with prettier

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -3,16 +3,16 @@ import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-	constructor(private userService: UsersService) {}
+  constructor(private userService: UsersService) {}
 
-	async validateUser(username: string, password: string): Promise<any> {
-		const user = await this.userService.findOne(username);
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(username);
 
-		if (user && user.password === password) {
-			const { password, username, ...rest } = user;
-			return rest;
-		}
+    if (user && user.password === password) {
+      const { password, username, ...rest } = user;
+      return rest;
+    }
 
-		return null;
-	}
+    return null;
+  }
 }
