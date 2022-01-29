ca40f98990b055b3ea6a7710a2a7a8e711a308c9
johnyworld <johnyworld@naver.com>
2022-01-29 22:37:49 +0900

    Add SuccessInterceptor

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './http-exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from './common/intercepters/success.intercepter';
 
 const corsOptions = {
   origin: true,
@@ -12,6 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new SuccessInterceptor());
   app.enableCors(corsOptions);
   const PORT = process.env.PORT;
   await app.listen(PORT);
