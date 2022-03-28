426dfe3748e605d57d37c18507ad64daefdb63e0
Sangimi <hsi3020@naver.com>
2022-03-28 21:37:55 +0900

    Login components arrangement

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -3,16 +3,18 @@ import api from './utils/api';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser } from './stores/userSlice';
 import './Login.scss';
-import { Route, Routes, Link } from 'react-router-dom';
+
 import useKakaoLogin from './hooks/useKakaoLogin';
 import { logOut } from './stores/userSlice';
-import LoginForm from './components/LoginForm';
+import LoginForm from './components/Login/LoginForm';
+import Logo from './components/Login/Logo';
+import LoginChannel from './components/Login/LoginChannel';
+import LoginSNS from './components/Login/LoginSNS';
 
 export default function Login() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const user = useSelector(state => state.user.userInfo);
   const kakaoLogin = useKakaoLogin();
   const handleLogout = () => {
     dispatch(logOut());
@@ -31,52 +33,12 @@ export default function Login() {
 
   return (
     <div className='Login'>
-      <h2>
-        YULMAE<br></br> MARKET
-      </h2>
+      <Logo />
 
       <LoginForm onSubmit={handler} />
 
-      <div className='login-route'>
-        <Link to='/join' className='text-link'>
-          회원가입
-        </Link>
-        <p>|</p>
-        <Link to='' className='text-link'>
-          아이디찾기
-        </Link>
-        <p>|</p>
-        <Link to='' className='text-link'>
-          비밀번호찾기
-        </Link>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </div>
-      <div className='login-sns'>
-        <p>
-          <b>SNS계정으로 로그인하기</b>
-        </p>
-        <div className='btn-sns-wrap'>
-          {user ? (
-            <button type='button' onClick={handleLogout}>
-              로그아웃
-            </button>
-          ) : (
-            <div className='btn-sns'>
-              <button type='button' onClick={() => {}}>
-                <img className='img-sns' src={require('./img/btn-naver.png')} />
-              </button>
-              <button type='button' onClick={kakaoLogin}>
-                <img className='img-sns' src={require('./img/btn-kakao.png')} />
-              </button>
-              <button type='button' onClick={() => {}}>
-                <img className='img-sns' src={require('./img/btn-google.png')} />
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
+      <LoginChannel />
+      <LoginSNS onSubmit={(kakaoLogin, handleLogout)} />
     </div>
   );
 }
---
+++ client/src/components/Login/LoginSNS.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './LoginSNS.scss';
+
+function LoginSNS({ onSubmit }) {
+  const user = useSelector(state => state.user.userInfo);
+  const submitHandler = e => {
+    e.preventDefault();
+    console.log(user);
+    onSubmit(user);
+  };
+
+  return (
+    <div className='login-sns'>
+      <p>
+        <b>SNS계정으로 로그인하기</b>
+      </p>
+      <div className='btn-sns-wrap'>
+        {user ? (
+          <button type='button' onSubmit={submitHandler}>
+            로그아웃
+          </button>
+        ) : (
+          <div className='btn-sns'>
+            <button type='button' onClick={() => {}}>
+              <img className='img-sns' src={require('../../img/btn-naver.png')} />
+            </button>
+            <button type='button' onClick={() => {}}>
+              <img className='img-sns' src={require('../../img/btn-kakao.png')} />
+            </button>
+            <button type='button' onClick={() => {}}>
+              <img className='img-sns' src={require('../../img/btn-google.png')} />
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default LoginSNS;
